197336758d94e0bdaf1ae821293a0910ef7bd8e1
edwhinr <edwhinr@gmail.com>
2024-07-26 23:24:57 +0700

    new content for readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/ud456hEF)
 # assignment-module-3
+
+## Checkpoint #1 
+
+<h2> Skill set</h2>
+
+1. JavaScript Integration
+2. JavaScript DOM and Events:
+
+<h2>Assignment Information</h2>
+
+1. crate new element for content
+2. give class name for new element
+3. styling img, title, and instructions
+4. make ul for instructions and remove list style
--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ const recipe = {
     instructions:
       "1. Cook spaghetti according to package instructions. 2. In a separate pan, cook pancetta until crispy. 3. In a bowl, whisk eggs and grated cheese. 4. Combine spaghetti, pancetta, and egg mixture, stirring quickly. 5. Serve immediately with additional cheese and black pepper.",
     image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
-  };
+};
   
   const recipeBtn = document.getElementById("getRecipeBtn")
   
   recipeBtn.addEventListener("click", addContent);
   
-  function addContent() {
+function addContent() {
   // make create element
   const takeContent = document.getElementById("recipeContainer");
   const titl = document.createElement("H1");
@@ -42,7 +42,7 @@ const recipe = {
   titl.style.textAlign = "center";
 
   // make list for instructions
-function convertToUl(listinstructions){
+  function convertToUl(listinstructions){
     // Use a regular expression to match instructions between numbers
     const steps = listinstructions.match(/(?:\d+\.\s)(.*?)(?=\s*\d+\.\s|$)/g);
     
@@ -51,11 +51,11 @@ function convertToUl(listinstructions){
 
     // Wrap the list items in <ul> tags
     return `<ul style="list-style-type: none;">${listItems}</ul>`;
-}
-const ulInstructions = convertToUl(recipe.instructions);
+  }
+  const ulInstructions = convertToUl(recipe.instructions);
 
-inst.innerHTML = ulInstructions;
+  inst.innerHTML = ulInstructions;
   
   const removeBtn = document.getElementById("getRecipeBtn")
   removeBtn.remove();
-  }
+}
