2ee292f9250976027556f941e6b9ba9c88b84589
edwhinr <edwhinr@gmail.com>
2024-08-09 22:13:42 +0700

    update content
    update content for final checkpoint

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # assignment-module-3
 
-## Checkpoint #1 
+## Final Checkpoint 
 
 <h2> Skill set</h2>
 
-1. JavaScript Integration
-2. JavaScript DOM and Events:
+Understanding TypeScript's Nature and Syntax:
+1. Converts the JavaScript file to TypeScript, ensuring full compatibility with TypeScript's features and syntax.
+
+2. Utilizes TypeScript's type annotations, interfaces, and type inference effectively to enhance code readability and maintainability.
+
+3. Ensures that the TypeScript code maintains the functionality and structure of the original JavaScript implementation.
 
 <h2>Assignment Information</h2>
 
-1. crate new element for content
-2. give class name for new element
-3. styling element in javascript
-4. add br after . (dot) in instructions
+1. Understanding the TypeScript Functions
+2. Implementing Core TypeScript Functions
+3. Finalizing and Submitting
 
 ## Link : https://revou-fsse-5.github.io/module-3-edwhinr/
---
+++ script.ts
@@ -0,0 +1,60 @@
+// take element by id and declare as html div element
+const takeContentById = document.getElementById("recipeContainer");
+const getMainContent = takeContentById as HTMLDivElement;
+
+// make create element
+const image = document.createElement("IMG");
+const title = document.createElement("H1");
+const instructions = document.createElement("p");
+
+// append element
+getMainContent.appendChild(image);
+getMainContent.appendChild(title);
+getMainContent.appendChild(instructions);
+
+const recipeBtnById = document.getElementById("getRecipeBtn")
+const recipeBtnLoad = recipeBtnById as HTMLButtonElement;
+
+recipeBtnLoad.addEventListener("click", addRandomRecipeCnt);
+
+async function addRandomRecipeCnt(): Promise<string> {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+        const data = await response.json();
+        const recipe = data.meals[0];
+
+        // take content from Constant Objects
+        image.setAttribute('src', recipe.strMealThumb);
+        title.innerHTML = recipe.strMeal;
+        instructions.innerHTML = recipe.strInstructions;
+        
+        // add br after . (dot)
+        const textInstructions = recipe.strInstructions;
+        const newTextInstructions = textInstructions.replace(/\. /g, '.<br>')
+        
+        instructions.innerHTML = newTextInstructions;
+        return 'Recipe loaded succesfully';
+    } catch (error) {
+        console.error('Error fetching the recipe:', error);
+        return 'Error loading recipe'
+    }
+}
+
+// give class name
+image.className = "recipe-image"
+title.className = "recipe-title"
+instructions.className = "recipe-instructions"
+
+// style image
+recipeBtnLoad.addEventListener("click", function(){
+    image.style.width = "300px";
+    image.style.height = "300px";
+    image.style.display = "block";
+    image.style.marginLeft =  "auto";
+    image.style.marginRight =  "auto";
+
+    instructions.style.textAlign = "center"
+})
+
+// style title
+title.style.textAlign = "center";
